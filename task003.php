4bf1907af5307a724a6056d329fcf1d545cc59fc
Slavisa Kujundzic <Slavisa3@users.noreply.github.com>
2016-08-28 22:53:16 +0200

    Update task003.php

--- task003.php
+++ task003.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$arr = array('mleko',.','. 'hleb', 'voda','mleko', 'voda', 'mleko', 'hleb');
+$arr = array('mleko', 'hleb', 'voda','mleko', 'voda', 'mleko', 'hleb');
 
 $rez = array_count_values($arr);
 asort($rez);
